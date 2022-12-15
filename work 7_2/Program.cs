178f3405c2eb3c8faf1896de7480e31546c5163d
Anvar <anv@list.com>
2022-12-16 00:37:37 +0400

    задание2 (поиск элемента массива с выводом массива)

--- work 7_2/Program.cs
+++ work 7_2/Program.cs
@@ -13,14 +13,6 @@ for (int i = 0; i < matrix.GetLength(0); i++)
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
         matrix[i,j] = new Random().Next(1,10);
-    }  
-Console.WriteLine();
-}
-
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
         Console.Write($"{matrix[i,j]}");
     }  
 Console.WriteLine();
