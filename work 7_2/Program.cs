693086abbc0a3e581a6f590239a42e5ebfa3511a
Anvar <anv@list.com>
2022-12-16 00:34:06 +0400

    задание2 (поиск элемента в массиве + вывод массива)

--- work 7_2/Program.cs
+++ work 7_2/Program.cs
@@ -7,14 +7,21 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-
-
 int [,] matrix = new int [3,4];
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
         matrix[i,j] = new Random().Next(1,10);
+    }  
+Console.WriteLine();
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i,j]}");
     }  
 Console.WriteLine();
 }
