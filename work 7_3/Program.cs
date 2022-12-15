5579ca1bc21c55be568c5d14009db0960229be7e
Anvar <anv@list.com>
2022-12-16 02:16:45 +0400

    среднее арифм каждого столбца двумерного массива

---
+++ work 7_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int [,] matrix = new int [2,4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1,10);
+        Console.Write(matrix[i,j]+" ");
+        //Console.Write($"{matrix[i,j]+" "}");
+    }  
+Console.WriteLine();
+}
+
+Console.Write("среднее арифметическое каждого столбца: ");
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    double sredn=0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum = sum+matrix[i,j];
+    }
+    sredn=sum/matrix.GetLength(0);
+    Console.Write(sredn+" ");
+    sum=0;
+    sredn=0;
+}
+Console.WriteLine();
